737f3b6290efd0dc79b1d16df75e4cd429ffebbb
Dimas Nuryadin <dimasnuryadinn@gmail.com>
2025-01-14 12:48:02 +0700

    create API get users

--- routes/handler/users/index.js
+++ routes/handler/users/index.js
@@ -1,9 +1,11 @@
 const register = require('./register');
 const login = require('./login')
 const update = require('./update')
+const getUsers = require('./getUsers')
 
 module.exports = {
   register,
   login,
-  update
+  update,
+  getUsers
 }
